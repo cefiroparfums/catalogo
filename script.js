706161bd9245ada7a453af18a551c0f78d99b22c
cefiroparfums <parfumscefiro@gmail.com>
2025-08-07 10:53:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,258 @@
 const perfumeData = [
-    {
-        "nombre_comercial": "VALENTINO DONNA",
-        "casa_fabricante": "Valentino",
-        "notas": {
-            "salida": "",
-            "corazon": "",
-            "fondo": "Notas atalcadas, avainilladas"
-        },
-        "genero": "Femenino",
-        "imagen_url": "https://www.perfumesbogota.com.co/cdn/shop/products/VALENTINO_DONNA_BORN_IN_ROMA_1024x1024@2x.webp?v=1677722034"
+        
+        {
+    "nombre_comercial": "VALENTINO DONNA",
+    "casa_fabricante": "Valentino",
+    "notas": {
+      "salida": "",
+      "corazon": "",
+      "fondo": "Notas atalcadas, avainilladas"
     },
-    // ... (todos los demás perfumes del catálogo)
+    "genero": "Femenino",
+    "imagen_url": "https://www.perfumesbogota.com.co/cdn/shop/products/VALENTINO_DONNA_BORN_IN_ROMA_1024x1024@2x.webp?v=1677722034"
+  },
+  {
+    "nombre_comercial": "BLEECKER STREET",
+    "casa_fabricante": "Bond No. 9",
+    "notas": {
+      "salida": "Hojas de violeta, arándano, tomillo",
+      "corazon": "Cedro, jazmín, vainilla, canela",
+      "fondo": "Musgo de roble, gamuza, pachulí, ámbar"
+    },
+    "genero": "Unisex",
+    "imagen_url": "https://perfumescolombia.com.co/cdn/shop/files/bond-n9-bleecker-street-karol-g-100ml-edp-11-premium-4477199.webp?v=1751883931"
+  },
+  {
+    "nombre_comercial": "FLOWERBOMB",
+    "casa_fabricante": "Viktor & Rolf",
+    "notas": {
+      "salida": "Osmanthus, té, bergamota",
+      "corazon": "Orquídea, rosa, fresia, flor de naranjo africano, jazmín",
+      "fondo": "Pachulí, almizcle"
+    },
+    "genero": "Femenino",
+    "imagen_url": "https://m.media-amazon.com/images/I/61RWULJE6+L._SX522_.jpg"
+  },
+  {
+    "nombre_comercial": "ECLAIRE",
+    "casa_fabricante": "Lattafa",
+    "notas": {
+      "salida": "Caramelo, leche, azúcar",
+      "corazon": "Flores blancas, miel",
+      "fondo": "Vainilla, praliné, almizcle"
+    },
+    "genero": "Femenino",
+    "imagen_url": "https://http2.mlstatic.com/D_NQ_NP_792591-MLA84559080116_052025-O.webp"
+  },
+  {
+    "nombre_comercial": "YARA MOI",
+    "casa_fabricante": "Lattafa",
+    "notas": {
+      "salida": "",
+      "corazon": "",
+      "fondo": ""
+    },
+    "genero": "Femenino",
+    "imagen_url": "https://veronnaperfumeria.com/cdn/shop/files/photo-output_130_1800x1800.heic.jpg?v=1718414496"
+  },
+  {
+    "nombre_comercial": "CLOUD",
+    "casa_fabricante": "Ariana Grande",
+    "notas": {
+      "salida": "Flor de lavanda, pera jugosa, bergamota",
+      "corazon": "Crema de coco, praliné, orquídea de vainilla",
+      "fondo": "Almizcles sensuales, maderas rubias cremosas"
+    },
+    "genero": "Femenino",
+    "imagen_url": "https://http2.mlstatic.com/D_NQ_NP_726719-MLU72836480957_112023-O.webp"
+  },
+  {
+    "nombre_comercial": "GOOD GIRL SUPREME",
+    "casa_fabricante": "Carolina Herrera",
+    "notas": {
+      "salida": "Bayas, jazmín",
+      "corazon": "Nardos, haba tonka tostada",
+      "fondo": "Vetiver"
+    },
+    "genero": "Femenino",
+    "imagen_url": "https://medipielsa.vtexassets.com/arquivos/ids/194648-800-auto?v=638617520790870000&width=800&height=auto&aspect=true"
+  },
+  {
+    "nombre_comercial": "212 VIP ROSE",
+    "casa_fabricante": "Carolina Herrera",
+    "notas": {
+      "salida": "Champagne Rosé, pimienta rosa",
+      "corazon": "Flor de durazno, bouquet de rosas",
+      "fondo": "Queenwood, almizcle"
+    },
+    "genero": "Femenino",
+    "imagen_url": "https://noirperfumeria.co/cdn/shop/files/212-vip-ROse-NP.webp?v=1682953708&width=713"
+  },
+  {
+    "nombre_comercial": "BRIGHT CRYSTAL",
+    "casa_fabricante": "Versace",
+    "notas": {
+      "salida": "Yuzu, granada",
+      "corazon": "Peonía, magnolia, flor de loto",
+      "fondo": "Almizcle, ámbar"
+    },
+    "genero": "Femenino",
+    "imagen_url": "https://static.wixstatic.com/media/7e4077_1c1412a8cc4c4dd4ab46ba24760f9920~mv2.png/v1/fill/w_500,h_500,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/7e4077_1c1412a8cc4c4dd4ab46ba24760f9920~mv2.png"
+  },
+  {
+    "nombre_comercial": "LA VIE EST BELLE",
+    "casa_fabricante": "Lancôme",
+    "notas": {
+      "salida": "Frambuesa, pimienta rosa, bergamota (Oui La Vie Est Belle); Grosella negra, pera (La Vie Est Belle original)",
+      "corazon": "Rosa, ylang-ylang (Oui La Vie Est Belle); Iris, jazmín, flor de azahar del naranjo (La Vie Est Belle original)",
+      "fondo": "Notas dulces, iris, pachulí (Oui La Vie Est Belle); Pachulí, haba tonka, vainilla, praliné (La Vie Est Belle original)"
+    },
+    "genero": "Femenino",
+    "imagen_url": "https://i5.walmartimages.com/asr/3d994134-2478-495e-b5ea-2244fcf4871c_2.a3c248744c84d72028dbee26ccf6f239.jpeg"
+  },
+  {
+    "nombre_comercial": "COCONUT PASSION",
+    "casa_fabricante": "Victoria's Secret",
+    "notas": {
+      "salida": "",
+      "corazon": "",
+      "fondo": "Vainilla, coco, notas dulces, lácteas, atalcadas, tropicales"
+    },
+    "genero": "Femenino",
+    "imagen_url": "https://http2.mlstatic.com/D_NQ_NP_857926-MLU73809168593_012024-O.webp"
+  },
+  {
+    "nombre_comercial": "BEAUTY RUSH JUICED BERRY",
+    "casa_fabricante": "Victoria's Secret",
+    "notas": {
+      "salida": "",
+      "corazon": "",
+      "fondo": "Varias bayas (fresas, frambuesas, moras), notas dulces y cítricas"
+    },
+    "genero": "Femenino",
+    "imagen_url": "https://m.media-amazon.com/images/I/61riQDFTskL._AC_SY606_.jpg"
+  },
+  {
+    "nombre_comercial": "9PM",
+    "casa_fabricante": "Afnan",
+    "notas": {
+      "salida": "Manzana, bergamota, lavanda, canela",
+      "corazon": "Flor de naranjo, lirio del valle",
+      "fondo": "Vainilla, haba tonka, ámbar, pachulí"
+    },
+    "genero": "Masculino",
+    "imagen_url": "https://http2.mlstatic.com/D_NQ_NP_861650-CBT82788540392_032025-O.webp"
+  },
+  {
+    "nombre_comercial": "VALENTINO UOMO BORN IN ROMA",
+    "casa_fabricante": "Valentino",
+    "notas": {
+      "salida": "Salvia",
+      "corazon": "Jengibre, sal mineral",
+      "fondo": "Vetiver, madera de guayacán"
+    },
+    "genero": "Masculino",
+    "imagen_url": "https://www.perfumesbogota.com.co/cdn/shop/products/D_NQ_NP_770818-MEC47398130236_092021-W_1024x1024@2x.jpg?v=1677779209"
+  },
+  {
+    "nombre_comercial": "LE BEAU",
+    "casa_fabricante": "Jean Paul Gaultier",
+    "notas": {
+      "salida": "Piña, iris, jengibre, ciprés",
+      "corazon": "Coco, notas amaderadas",
+      "fondo": "Ámbar, haba tonka"
+    },
+    "genero": "Masculino",
+    "imagen_url": "https://www.perfumesbogota.com.co/cdn/shop/products/le_beau_le_male_1024x1024@2x.jpg?v=1677779652"
+  },
+  {
+    "nombre_comercial": "ACQUA DI GIO PROFUMO",
+    "casa_fabricante": "Giorgio Armani",
+    "notas": {
+      "salida": "Notas marinas",
+      "corazon": "Romero, salvia, geranio",
+      "fondo": "Incienso, pachulí"
+    },
+    "genero": "Masculino",
+    "imagen_url": "https://perfumescolombia.com.co/cdn/shop/products/acqua-di-gio-profumo-11-premium-1959112.png?v=1751884239"
+  },
+  {
+    "nombre_comercial": "CLUB DE NUIT INTENSE MAN",
+    "casa_fabricante": "Armaf",
+    "notas": {
+      "salida": "Limón, piña, grosella negra, manzana, bergamota",
+      "corazon": "Abedul, jazmín, rosa",
+      "fondo": "Almizcle, ámbar gris, pachulí, vainilla"
+    },
+    "genero": "Masculino",
+    "imagen_url": "https://exitocol.vtexassets.com/arquivos/ids/23602302/perfume-armaf-club-de-nuit-intense-man-hombre-locion-105ml.jpg?v=638563955996100000"
+  },
+  {
+    "nombre_comercial": "BAD BOY",
+    "casa_fabricante": "Carolina Herrera",
+    "notas": {
+      "salida": "Bergamota, pimienta negra, pimienta blanca",
+      "corazon": "Cedro, salvia",
+      "fondo": "Haba tonka, cacao, ámbar"
+    },
+    "genero": "Masculino",
+    "imagen_url": "https://veronnaperfumeria.com/cdn/shop/products/image_1665b8a0-c275-47bc-a4b2-6bcf8531237d_750x.jpg?v=1655394718"
+  },
+  {
+    "nombre_comercial": "ONE MILLION LUCKY",
+    "casa_fabricante": "Paco Rabanne",
+    "notas": {
+      "salida": "Ciruela, notas ozónicas, pomelo, bergamota",
+      "corazon": "Avellana, miel, cedro, madera de cachemira, jazmín, flor de naranjo",
+      "fondo": "Amberwood, pachulí, musgo de roble, vetiver"
+    },
+    "genero": "Masculino",
+    "imagen_url": "https://luryx.com.co/pub/media/catalog/product/cache/17a2882685dcb778cb02abe71b33b15c/_/6/_6_5_65131403_2.jpg"
+  },
+  {
+    "nombre_comercial": "TERRE D HERMES",
+    "casa_fabricante": "Hermès",
+    "notas": {
+      "salida": "Naranja, pomelo",
+      "corazon": "Pimienta",
+      "fondo": "Vetiver, cedro, pachulí, benjuí"
+    },
+    "genero": "Masculino",
+    "imagen_url": "https://mwhite.com.co/cdn/shop/files/terre-de-hermes-parfum-200ml-perfumes-574.webp?v=1713450362&width=493"
+  },
+  {
+    "nombre_comercial": "INVICTUS VICTORY",
+    "casa_fabricante": "Paco Rabanne",
+    "notas": {
+      "salida": "Pimienta rosa, limón",
+      "corazon": "Incienso, lavanda",
+      "fondo": "Vainilla, haba tonka, ámbar"
+    },
+    "genero": "Masculino",
+    "imagen_url": "https://http2.mlstatic.com/D_NQ_NP_634002-MLA47844547674_102021-O.webp"
+  },
+  {
+    "nombre_comercial": "212 HEROES",
+    "casa_fabricante": "Carolina Herrera",
+    "notas": {
+      "salida": "Pera, cannabis, jengibre",
+      "corazon": "Geranio, salvia",
+      "fondo": "Almizcle, cuero"
+    },
+    "genero": "Masculino",
+    "imagen_url": "https://perfumescolombia.com.co/cdn/shop/products/212-men-heroes-11-premium-1064601.png?v=1751884226"
+  },
+  {
+    "nombre_comercial": "KHAMRAH QAHWA",
+    "casa_fabricante": "Lattafa",
+    "notas": {
+      "salida": "Cardamomo, jengibre, canela",
+      "corazon": "Praliné, flores blancas, frutas confitadas",
+      "fondo": "Café, haba tonka, benjuí, vainilla, almizcle"
+    },
+    "genero": "Unisex",
+    "imagen_url": "https://cdn.wholesale55.com/wp-content/uploads/2024/11/Khamrah-Qahwa-EDP-100-ml-Unisex-by-Lattafa-1.jpg.jpeg"
+  },  // ... (todos los demás perfumes del catálogo)
 ];
 
 let allPerfumes = [];
